feb087c2cf0c5539037e3a94fbfc8ac161a1ad1e
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-04-14 03:22:31 +0200

    Added solution for next laboratory - Multithreading Server Task

---
+++ NetworkProgramming.Lab4/Models/NetworkInterfaceModel.cs
@@ -0,0 +1,13 @@
+﻿namespace NetworkProgramming.Lab4.Models
+{
+   public class NetworkInterfaceModel
+   {
+      public string Name { get; set; }
+      public string Ip { get; set; }
+
+      public override string ToString()
+      {
+         return $"{Name} | {Ip}";
+      }
+   }
+}
---
+++ NetworkProgramming.Lab4/Views/ClientsGrid.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NetworkProgramming.Lab4.Views
+{
+   public class ClientsGrid : UserControl
+   {
+      public ClientsGrid()
+      {
+         this.InitializeComponent();
+      }
+
+      private void InitializeComponent()
+      {
+         AvaloniaXamlLoader.Load(this);
+      }
+   }
+}
