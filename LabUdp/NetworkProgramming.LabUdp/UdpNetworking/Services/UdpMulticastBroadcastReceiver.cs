34713bc3b33d15a15adbf2be422616c850cfdb50
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-01 22:28:18 +0200

    Added UdpBroadcastClient, UdpMulticastBroadcastReceiver and UdpMulticastClient. Due to the fact, that project from lab6 references previous laboratories, I renamed solution to NetworkProgramming.LabUdp

---
+++ LabUdp/NetworkProgramming.LabUdp/UdpNetworking/Services/UdpMulticastBroadcastReceiver.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UdpNetworking.Services.Enums;
+using UdpNetworking.Services.Extensions;
+using UdpNetworking.Services.States;
+
+namespace UdpNetworking.Services
+{
+   public class UdpMulticastBroadcastReceiver
+   {
+      private Socket _serverSocket;
+      private int _port;
+      private const int MaxLen = 1024;
+      private bool _acceptBroadcast;
+      private IPAddress _address;
+      private readonly Dictionary<EndPoint, ControlState> _clientsBuffers;
+
+      public event EventHandler<object[]> LogEvent;
+
+      public UdpMulticastBroadcastReceiver() => _clientsBuffers = new Dictionary<EndPoint, ControlState>();
+
+      public void InitSocket(string groupAddress, int port, bool acceptBroadcast)
+      {
+         LogEvent?.Invoke(this, new object[]
+         {
+            (int)LogLevels.Info, "Initializing socket"
+         });
+
+         try
+         {
+            _port = port;
+            _acceptBroadcast = acceptBroadcast;
+            _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
+            {
+               EnableBroadcast = _acceptBroadcast
+            };
+
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, _port));
+            var add = IPAddress.Parse(groupAddress);
+
+            _serverSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.ReuseAddress, 1);
+            _serverSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 255);
+            _serverSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(add));
+
+            _address = add;
+         }
+         catch (Exception e)
+         {
+            LogEvent?.Invoke(this, new object[]
+            {
+               (int)LogLevels.Error, e, "Cannot init socket properly"
+            });
+         }
+      }
+
+      public void StopService() => (_serverSocket == null || _serverSocket.IsDisposed() ? (Action)(() => { }) : () =>
+      {
+         _serverSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.DropMembership, _address);
+         _serverSocket.Close();
+      })();
+
+      public void Receive() => Receive(_serverSocket);
+
+      private void Receive(Socket socket)
+      {
+         if (socket.IsDisposed()) return;
+         var state = new ControlState()
+         {
+            CurrentSocket = socket,
+            BufferSize = MaxLen,
+            Buffer = new byte[MaxLen],
+            StreamBuffer = new MemoryStream()
+         };
+
+         LogEvent?.Invoke(this, new object[]
+         {
+            (int)LogLevels.Info, "Started transfer data from potential senders"
+         });
+
+
+         try
+         {
+            var endPoint = new IPEndPoint(IPAddress.Any, 0) as EndPoint;
+            socket.BeginReceiveFrom(state.Buffer, 0, state.Buffer.Length, 0, ref endPoint, ReceiveFromCallback, state);
+         }
+         catch (Exception e)
+         {
+            LogEvent?.Invoke(this, new object[]
+            {
+               (int)LogLevels.Error, e, "Failed to receive data"
+            });
+         }
+      }
+
+      private void ReceiveFromCallback(IAsyncResult ar)
+      {
+         try
+         {
+            var end = new IPEndPoint(IPAddress.Any, 0) as EndPoint;
+            if (!(ar.AsyncState is ControlState state)) return;
+            var bytesRead = state.CurrentSocket.EndReceiveFrom(ar, ref end);
+
+            if (!_clientsBuffers.ContainsKey(end))
+            {
+               var s = new ControlState
+               {
+                  Buffer = new byte[MaxLen],
+                  BufferSize = MaxLen,
+                  StreamBuffer = new MemoryStream(),
+               };
+               _clientsBuffers.Add(end, s);
+            }
+
+            if (bytesRead > 0)
+            {
+               _clientsBuffers[end].StreamBuffer.Write(state.Buffer, 0, bytesRead);
+               if (state.Buffer.Any(byte_ => byte_ == '\0'))
+               {
+                  ProcessMessage(end);
+                  _clientsBuffers[end].StreamBuffer = new MemoryStream();
+               }
+            }
+            else if (_clientsBuffers[end].StreamBuffer.CanWrite && _clientsBuffers[end].StreamBuffer.Length > 0)
+            {
+               ProcessMessage(end);
+               _clientsBuffers[end].StreamBuffer = new MemoryStream();
+            }
+
+            var e = new IPEndPoint(IPAddress.Any, 0) as EndPoint;
+            state.CurrentSocket.BeginReceiveFrom(state.Buffer, 0, state.Buffer.Length, SocketFlags.None, ref e,
+               ReceiveFromCallback, state);
+
+         }
+         catch (Exception e)
+         {
+            LogEvent?.Invoke(this, new object[]
+            {
+               (int)LogLevels.Error, e, "Failed to receive data"
+            });
+         }
+
+
+      }
+
+      private void ProcessMessage(EndPoint end)
+      {
+         if (!_clientsBuffers.ContainsKey(end)) return;
+         var state = _clientsBuffers[end];
+         using var stream = state.StreamBuffer;
+         stream.Seek(0, SeekOrigin.Begin);
+         var message = Encoding.UTF8.GetString(stream.ToArray());
+         LogEvent?.Invoke(this, new object[] { (int)LogLevels.Success, "Successfully received message" });
+         LogEvent?.Invoke(this, new object[] { (int)LogLevels.Client, end, message });
+      }
+   }
+}
---
+++ LabUdp/NetworkProgramming.LabUdp/UdpNetworking/Services/UdpMulticastClient.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UdpNetworking.Services.Enums;
+using UdpNetworking.Services.Extensions;
+
+namespace UdpNetworking.Services
+{
+   public class UdpMulticastClient
+   {
+      private Socket _socket;
+      private int _port;
+      private IPAddress _address;
+      public event EventHandler<object[]> LogEvent;
+
+      public void InitSocket(string address, int port)
+      {
+         LogEvent?.Invoke(this, new object[]
+         {
+            (int)LogLevels.Info, "Initializing socket"
+         });
+
+         try
+         {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            _port = port;
+            var add = IPAddress.Parse(address);
+
+            _socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.ReuseAddress, 1);
+            _socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 255);
+            _socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(add));
+
+            _address = add;
+         }
+         catch (Exception e)
+         {
+            LogEvent?.Invoke(this, new object[]
+            {
+               (int)LogLevels.Error, e, "Cannot init socket properly"
+            });
+         }
+      }
+
+      public void StopService() => (_socket == null || _socket.IsDisposed() ? (Action)(() => { }) : () =>
+      {
+         _socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.DropMembership, _address);
+         _socket.Close();
+      })();
+
+      public void Send(string msg) => Send(_socket, msg);
+
+      private void Send(Socket socket, string msg)
+      {
+         if (socket.IsDisposed()) return;
+         LogEvent?.Invoke(this, new object[]
+         {
+            (int)LogLevels.Info, "Started broadcast transfer data to receivers"
+         });
+
+         var data = Encoding.ASCII.GetBytes(msg);
+
+         try
+         {
+            var endPoint = new IPEndPoint(_address, _port);
+            socket.BeginSendTo(data, 0, data.Length, SocketFlags.Broadcast, endPoint, SendToCallback, socket);
+         }
+         catch (Exception e)
+         {
+            LogEvent?.Invoke(this, new object[]
+            {
+               (int)LogLevels.Error, e, "Failed to send data"
+            });
+         }
+      }
+
+      private void SendToCallback(IAsyncResult ar)
+      {
+         if (!(ar.AsyncState is Socket socket)) return;
+
+         try
+         {
+            var _ = socket.EndSendTo(ar);
+            LogEvent?.Invoke(this, new object[]
+            {
+               (int)LogLevels.Success, "Data were successfully multicast"
+            });
+         }
+         catch (Exception e)
+         {
+            LogEvent?.Invoke(this, new object[]
+            {
+               (int)LogLevels.Error, e, "Failed to send data"
+            });
+         }
+      }
+   }
+}
