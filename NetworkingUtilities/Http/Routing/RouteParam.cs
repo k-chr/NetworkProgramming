64b6772f5971dd5ec53874dc81e941a46ae8e4ea
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-11 19:06:07 +0200

    Added new class, RouteParam is inidicated in url by {}  e.x. Home/{controller}/{param:int=0}

---
+++ NetworkingUtilities/Http/Routing/RouteParam.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetworkingUtilities.Http.Routing
+{
+	public class RouteParam : IRouteElement
+	{
+
+		public RouteParam(string key, int id, bool optional, Dictionary<string, Func<object, bool>> constraints, IEnumerable<string> defaults)
+		{
+			Key = key;
+			Id = id;
+			Optional = optional;
+			Constraints = constraints;
+			Defaults = defaults;
+		}
+
+		public string Key { get; }
+		public int Id { get; }
+		public bool Optional { get; }
+		public Dictionary<string, Func<object, bool>> Constraints { get; }
+		public IEnumerable<string> Defaults { get; }
+	}
+}
