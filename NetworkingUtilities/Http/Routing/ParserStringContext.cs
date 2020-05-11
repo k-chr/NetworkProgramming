f4925f0ee064f91a32b05b4cdb41373a7b61af19
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-11 17:03:27 +0200

    Prepared simple string utility to ease parsing process

---
+++ NetworkingUtilities/Http/Routing/ParserStringContext.cs
@@ -0,0 +1,36 @@
+﻿using NetworkingUtilities.Extensions;
+
+namespace NetworkingUtilities.Http.Routing
+{
+	internal class ParserStringContext
+	{
+		private readonly string _data;
+		private int _index;
+		private int? _captureStart;
+
+		public ParserStringContext(string data)
+		{
+			_data = data;
+			_index = -1;
+			_captureStart = null;
+		}
+
+		public char CurrentChar => (char) (_index.InRange(0, _data.Length - 1) ? _data[_index] : 0);
+
+		public string GetMark()
+		{
+			if (!_captureStart.HasValue) return null;
+			string d = _data[_captureStart.Value.._index];
+			_captureStart = null;
+			return d;
+		}
+
+		public void Mark() => _captureStart = _index;
+
+		public bool Next() => ++_index < _data.Length;
+
+		public bool Previous() => --_index >= 0;
+
+		public bool HasNext() => _index < _data.Length + 1;
+	}
+}
