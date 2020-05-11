72a11c4e19034e55cf2f1a4fb6e56d7b5cfa85b6
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-11 16:58:37 +0200

    Added literal pattern used, for instance, in /Home/{controller}/{anotherParam} where Home is literal for URI

---
+++ NetworkingUtilities/Extensions/RouteLiteral.cs
@@ -0,0 +1,14 @@
+﻿namespace NetworkingUtilities.Extensions
+{
+	public class RouteLiteral
+	{
+		public int Id { get; }
+		public string Key { get; }
+
+		public RouteLiteral(string key, int id)
+		{
+			Key = key;
+			Id = id;
+		}
+	}
+}
