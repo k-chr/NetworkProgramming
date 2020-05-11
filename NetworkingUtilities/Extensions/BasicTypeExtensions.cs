573f77457487e301389a1fbab54deab3b70075ff
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-11 16:49:18 +0200

    Added extension method for int

---
+++ NetworkingUtilities/Extensions/BasicTypeExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace NetworkingUtilities.Extensions
+{
+	public static class BasicTypeExtensions
+	{
+		public static bool InRange(this int testedValue, int min, int max) => testedValue >= Math.Min(min, max) && testedValue <= Math.Max(min, max);
+	}
+}
