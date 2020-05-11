fce0684f811c8789a654b787b7958bdbdb0f7f2b
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-11 16:50:46 +0200

    Added basic version of route pattern for future HTTP controller routing.

---
+++ NetworkingUtilities/Http/Routing/RoutePattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetworkingUtilities.Http.Routing
+{
+	public class RoutePattern
+	{
+		private string _baseData;
+
+		public RoutePattern(string baseData)
+		{
+			_baseData = baseData;
+		}
+	}
+}
