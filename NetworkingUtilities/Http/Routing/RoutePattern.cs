808c8dfee30f608e4775986065e2b764270f5852
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-11 18:25:50 +0200

    updated RoutePattern - added collection of IRouteElement to perform pattern matching in router or built endpoint.

--- NetworkingUtilities/Http/Routing/RoutePattern.cs
+++ NetworkingUtilities/Http/Routing/RoutePattern.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
 
 namespace NetworkingUtilities.Http.Routing
 {
 	public class RoutePattern
 	{
 		private string _baseData;
+		private readonly ICollection<IRouteElement> _routeElems;
 
-		public RoutePattern(string baseData)
+		public RoutePattern(string baseData, ICollection<IRouteElement> elems)
 		{
 			_baseData = baseData;
+			_routeElems = elems;
 		}
+
+		public ICollection<IRouteElement> RouteElems =>(_routeElems).ToImmutableList();
 	}
 }
