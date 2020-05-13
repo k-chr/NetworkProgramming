32f0158b93f8535ad93dd16160014dadb1130e94
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-13 22:38:40 +0200

    Filled jsons with data

--- HttpStorehouse/Models/ProductModel.cs
+++ HttpStorehouse/Models/ProductModel.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace HttpStorehouse.Models
 {
-	class ProductModel : IModel<int, string, string>
+	public class ProductModel : IModel<int, string, string>
 	{
 		public int Key { get; set; }
 		public string Value { get; set; }
