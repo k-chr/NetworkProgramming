4034afd8c4faac561c0985e266cab73f654dde8f
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-13 14:10:59 +0200

    Implemented args converter, however optional and defaults are not supported yet.

--- NetworkingUtilities/Http/Routing/HttpEndPoint.cs
+++ NetworkingUtilities/Http/Routing/HttpEndPoint.cs
@@ -35,7 +35,91 @@ namespace NetworkingUtilities.Http.Routing
 
 				dict = dict.Where(pair => names.Contains(pair.Key)).ToDictionary(pair => pair.Key, pair => pair.Value);
 
+				foreach (var (key, value) in dict)
+				{
+					var segment = patternSegments.FirstOrDefault(element => element.Key.Equals(key));
+					if (segment != null)
+					{
+						if (segment is RouteParam param)
+						{
+							var constraints = param.Constraints;
+							if (constraints.ContainsKey("intRange"))
+							{
+								var valueObject = value.Split('_').Select(int.Parse).ToArray();
+								args.Add(valueObject);
+							}
 
+							else if (constraints.ContainsKey("int"))
+							{
+								var valueObject = int.Parse(value);
+
+								Func<object, bool> func;
+								var test = true;
+								
+								if (constraints.ContainsKey("min"))
+								{
+									func = constraints["min"];
+									test = func(valueObject);
+								}
+
+								if (!test) return null;
+
+								if (constraints.ContainsKey("max"))
+								{
+									func = constraints["max"];
+									test = func(valueObject);
+								}
+
+								if (!test) return null;
+
+								if (constraints.ContainsKey("inRange"))
+								{
+									func = constraints["inRange"];
+									test = func(valueObject);
+								}
+
+								if (!test) return null;
+								args.Add(valueObject);
+							}
+
+							else if (constraints.ContainsKey("alpha"))
+							{
+								var test = value.All(char.IsLetter);
+
+								if (constraints.ContainsKey("length") && test)
+								{
+									var func = constraints["inRange"];
+									test = func(value);
+								}
+
+								if (!test) return null;
+								args.Add(value);
+							}
+
+							else if (constraints.ContainsKey("length"))
+							{
+								var func = constraints["inRange"];
+								var test = func(value);
+								if (!test) return null;
+								args.Add(value);
+							}
+
+							else if (constraints.ContainsKey("date"))
+							{
+								var test = DateTime.TryParse(value, out var val);
+								if (test)
+								{
+									args.Add(val);
+								}
+							}
+
+							else
+							{
+								args.Add(value);
+							}
+						}
+					}
+				}
 
 				var obj = _targetMethod.Invoke(_instanceOfController, args.ToArray());
 				if (obj is string s) return s;
@@ -62,7 +146,7 @@ namespace NetworkingUtilities.Http.Routing
 			{
 				foreach (var routeElement in patternSegments.Skip(segments.Length))
 				{
-					if (routeElement is RouteLiteral l || (routeElement is RouteParam p && !p.Optional))
+					if (routeElement is RouteLiteral || (routeElement is RouteParam p && !p.Optional))
 					{
 						rV = false;
 						break;
