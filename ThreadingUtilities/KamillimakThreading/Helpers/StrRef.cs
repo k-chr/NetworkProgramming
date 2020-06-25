f17424d8a87f5e7b06a6fa14f300da3c5c257568
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-26 00:23:38 +0200

    Moved ThreadingUtilities to main directory

---
+++ ThreadingUtilities/KamillimakThreading/Helpers/StrRef.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ThreadingUtilities.KamillimakThreading.Helpers
+{
+   public class StrRef
+   {
+      public string Value { get; set; }
+      public string Name { get; }
+      public static implicit operator StrRef(Tuple<string, string> tuple)
+      {
+         var (name, other) = tuple;
+         return new StrRef(name) { Value = other };
+      }
+
+      public StrRef(string name)
+      {
+         Name = name;
+      }
+      public override string ToString()
+      {
+         return Value;
+      }
+
+      public StrRef Replace(string old, string @new)
+      {
+         return (this.Name, this.Value.Replace(old, @new)).ToTuple();
+      }
+
+   }
+}
