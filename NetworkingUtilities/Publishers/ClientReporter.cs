0e2a77391d67a5458ffd342e1bcbdedeff8d7d91
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-07-08 20:22:18 +0200

    Updated Reporters

--- NetworkingUtilities/Publishers/ClientReporter.cs
+++ NetworkingUtilities/Publishers/ClientReporter.cs
@@ -7,17 +7,14 @@ namespace NetworkingUtilities.Publishers
 {
 	public class ClientReporter : IReporter, IReporter<ClientEvent>
 	{
+		public event EventHandler<ClientEvent> Report;
+
 		public void Notify(object obj)
 		{
-			if (obj is Tuple<IPAddress, string, int> data)
+			if (obj is ValueTuple<IPAddress, string, int> data)
 				Report?.Invoke(this, new ClientEvent(data.Item1, data.Item3, data.Item2));
 		}
 
-		public void AddSubscriber(Action<object, object> procedure)
-		{
-			Report += (s, e) => procedure?.Invoke(s, e);
-		}
-
-		public event EventHandler<ClientEvent> Report;
+		public void AddSubscriber(Action<object, object> procedure) => Report += (s, e) => procedure?.Invoke(s, e);
 	}
 }
