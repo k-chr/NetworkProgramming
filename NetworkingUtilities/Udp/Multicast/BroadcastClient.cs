8ce2ede179f6fa8f29fb2531063318a2fe662f95
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-07-12 13:36:56 +0200

    Added basis of UDP clients.

---
+++ NetworkingUtilities/Udp/Multicast/BroadcastClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Sockets;
+using NetworkingUtilities.Abstracts;
+
+namespace NetworkingUtilities.Udp.Multicast
+{
+   class BroadcastClient : AbstractClient
+   {
+	   public BroadcastClient(Socket clientSocket, bool serverHandler = false) : base(clientSocket, serverHandler)
+	   {
+	   }
+
+	   public override void Send(string message, string to = "")
+	   {
+		   throw new NotImplementedException();
+	   }
+
+	   public override void Receive()
+	   {
+		   throw new NotImplementedException();
+	   }
+
+	   public override void StopService()
+	   {
+		   throw new NotImplementedException();
+	   }
+
+	   public override void StartService()
+	   {
+		   throw new NotImplementedException();
+	   }
+   }
+}
---
+++ NetworkingUtilities/Udp/Multicast/MulticastClient.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Sockets;
+using NetworkingUtilities.Abstracts;
+
+namespace NetworkingUtilities.Udp.Multicast
+{
+   class MulticastClient : AbstractClient
+   {
+	   public MulticastClient(Socket clientSocket, bool serverHandler = false) : base(clientSocket, serverHandler)
+	   {
+	   }
+
+	   public override void Send(string message, string to = "")
+	   {
+		   throw new System.NotImplementedException();
+	   }
+
+	   public override void Receive()
+	   {
+		   throw new System.NotImplementedException();
+	   }
+
+	   public override void StopService()
+	   {
+		   throw new System.NotImplementedException();
+	   }
+
+	   public override void StartService()
+	   {
+		   throw new System.NotImplementedException();
+	   }
+   }
+}
