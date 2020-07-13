9b985d7523cefe52a77e0e0d7fd02d2a003f9ea2
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-07-13 23:50:31 +0200

    Added two constructors for UnicastClient

--- NetworkingUtilities/Udp/Unicast/UnicastClient.cs
+++ NetworkingUtilities/Udp/Unicast/UnicastClient.cs
@@ -1,33 +1,45 @@
 ﻿using System;
+using System.Net;
 using System.Net.Sockets;
 using NetworkingUtilities.Abstracts;
 
 namespace NetworkingUtilities.Udp.Unicast
 {
-   class UnicastClient : AbstractClient
-   {
-	   public UnicastClient(Socket clientSocket, bool serverHandler = false) : base(clientSocket, serverHandler)
-	   {
-	   }
+	class UnicastClient : AbstractClient
+	{
+		private IPEndPoint _endPoint;
+		private bool _listening;
 
-	   public override void Send(string message, string to = "")
-	   {
-		   throw new NotImplementedException();
-	   }
+		public UnicastClient(IPEndPoint endPoint, Socket clientSocket, bool serverHandler = false) : base(clientSocket, serverHandler)
+		{
+			_endPoint = endPoint;
+		}
 
-	   public override void Receive()
-	   {
-		   throw new NotImplementedException();
-	   }
+		public UnicastClient(string ip, int port) : base(new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+			ProtocolType.Udp))
+		{
+			var address = IPAddress.Parse(ip);
+			_endPoint = new IPEndPoint(address, port);
+		}
 
-	   public override void StopService()
-	   {
-		   throw new NotImplementedException();
-	   }
+		public override void Send(string message, string to = "")
+		{
+			throw new NotImplementedException();
+		}
 
-	   public override void StartService()
-	   {
-		   throw new NotImplementedException();
-	   }
-   }
+		public override void Receive()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void StopService()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void StartService()
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
