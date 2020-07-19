28d56ccb6fd1f83feb8e50b9feb26c5b7460bb2a
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-07-20 00:12:32 +0200

    implemented constructor for BroadcastClient, added SetBroadcastIp method.

--- NetworkingUtilities/Udp/Multicast/BroadcastClient.cs
+++ NetworkingUtilities/Udp/Multicast/BroadcastClient.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.Net;
 using System.Net.Sockets;
 using NetworkingUtilities.Abstracts;
 using NetworkingUtilities.Extensions;
-
+using static NetworkingUtilities.Utilities.BroadcastTools;
 namespace NetworkingUtilities.Udp.Multicast
 {
 	public class BroadcastClient : AbstractClient
 	{
-		public BroadcastClient(Socket clientSocket, bool serverHandler = false) : base(clientSocket, serverHandler)
+		private readonly int _port;
+		private readonly string _ipAddress;
+		private readonly int _localPort;
+		private IPAddress _address;
+
+		public BroadcastClient(string interfaceIp, int port, bool serverHandler = false,
+			string ipAddress = null, int localPort = 0) : base(new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp), serverHandler)
 		{
+			_port = port;
+			_ipAddress = ipAddress;
+			_localPort = localPort;
+			SetBroadcastIp(interfaceIp);
 		}
 
 		public override void Send(string message, string to = "")
@@ -29,5 +40,7 @@ namespace NetworkingUtilities.Udp.Multicast
 		{
 			throw new NotImplementedException();
 		}
+
+		private void SetBroadcastIp(string selectedIp) => _address = GetBroadcastIpForAddress(selectedIp);
 	}
 }
