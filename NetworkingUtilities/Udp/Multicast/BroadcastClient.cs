ea54ca90927343fbc5643d9f09e53ace4d6f301b
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-07-20 00:27:45 +0200

    Implemented StartService, Receive, added OnReceiveFromCallback

--- NetworkingUtilities/Udp/Multicast/BroadcastClient.cs
+++ NetworkingUtilities/Udp/Multicast/BroadcastClient.cs
@@ -1,23 +1,29 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using NetworkingUtilities.Abstracts;
 using NetworkingUtilities.Extensions;
+using NetworkingUtilities.Utilities.Events;
+using NetworkingUtilities.Utilities.StateObjects;
 using static NetworkingUtilities.Utilities.BroadcastTools;
+
 namespace NetworkingUtilities.Udp.Multicast
 {
 	public class BroadcastClient : AbstractClient
 	{
 		private readonly int _port;
-		private readonly string _ipAddress;
+		private readonly IPAddress _ipAddress;
 		private readonly int _localPort;
 		private IPAddress _address;
 
 		public BroadcastClient(string interfaceIp, int port, bool serverHandler = false,
-			string ipAddress = null, int localPort = 0) : base(new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp), serverHandler)
+			string ipAddress = null, int localPort = 0) : base(
+			new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp), serverHandler)
 		{
 			_port = port;
-			_ipAddress = ipAddress;
+			_ipAddress = string.IsNullOrEmpty(ipAddress) ? IPAddress.Any : IPAddress.Parse(ipAddress);
 			_localPort = localPort;
 			SetBroadcastIp(interfaceIp);
 		}
@@ -29,7 +35,71 @@ namespace NetworkingUtilities.Udp.Multicast
 
 		public override void Receive()
 		{
-			throw new NotImplementedException();
+			try
+			{
+				var state = new ControlState
+				{
+					CurrentSocket = ClientSocket,
+					Buffer = new byte[MaxBufferSize],
+					BufferSize = MaxBufferSize,
+					StreamBuffer = new MemoryStream()
+				};
+				var ep = new IPEndPoint(IPAddress.Any, 0) as EndPoint;
+				ClientSocket.BeginReceiveFrom(state.Buffer, 0, MaxBufferSize, 0, ref ep, OnReceiveFromCallback, state);
+			}
+			catch (ObjectDisposedException)
+			{
+			}
+			catch (SocketException socketException)
+			{
+				OnCaughtException(socketException, EventCode.Receive);
+			}
+			catch (Exception e)
+			{
+				OnCaughtException(e, EventCode.Other);
+			}
+		}
+
+		private void OnReceiveFromCallback(IAsyncResult ar)
+		{
+			try
+			{
+				if (ar.AsyncState is ControlState state)
+				{
+					var ep = new IPEndPoint(IPAddress.Any, 0) as EndPoint;
+
+					var bytesRead = state.CurrentSocket.EndReceiveFrom(ar, ref ep);
+					if (bytesRead > 0)
+					{
+						state.StreamBuffer.Write(state.Buffer, 0, bytesRead);
+						state.Buffer = new byte[MaxBufferSize];
+
+						if (state.Buffer.Any(@byte => @byte == '\0'))
+						{
+							ProcessMessage(state.StreamBuffer);
+							state.StreamBuffer = new MemoryStream();
+						}
+					}
+					else if (state.StreamBuffer.CanWrite && state.StreamBuffer.Length > 0)
+					{
+						ProcessMessage(state.StreamBuffer);
+						state.StreamBuffer = new MemoryStream();
+					}
+
+					Receive();
+				}
+			}
+			catch (ObjectDisposedException)
+			{
+			}
+			catch (SocketException socketException)
+			{
+				OnCaughtException(socketException, EventCode.Receive);
+			}
+			catch (Exception e)
+			{
+				OnCaughtException(e, EventCode.Other);
+			}
 		}
 
 		public override void StopService() => (ClientSocket == null || ClientSocket.IsDisposed()
@@ -38,7 +108,30 @@ namespace NetworkingUtilities.Udp.Multicast
 
 		public override void StartService()
 		{
-			throw new NotImplementedException();
+			try
+			{
+				ClientSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+				ClientSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 32);
+				ClientSocket.Bind(new IPEndPoint(_ipAddress, _localPort));
+
+				if (ClientSocket.LocalEndPoint is IPEndPoint endPoint)
+				{
+					WhoAmI = new ClientEvent(endPoint.Address, endPoint.Port, WhoAmI.Id);
+				}
+
+				Receive();
+			}
+			catch (ObjectDisposedException)
+			{
+			}
+			catch (SocketException socketException)
+			{
+				OnCaughtException(socketException, EventCode.Bind);
+			}
+			catch (Exception exception)
+			{
+				OnCaughtException(exception, EventCode.Other);
+			}
 		}
 
 		private void SetBroadcastIp(string selectedIp) => _address = GetBroadcastIpForAddress(selectedIp);
