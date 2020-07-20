43b8fefd1011df61f0ab330dcd5c8e8921d7530b
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-07-20 23:40:49 +0200

    Added base of UnicastServer

---
+++ NetworkingUtilities/Udp/Unicast/UnicastServer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NetworkingUtilities.Abstracts;
+
+namespace NetworkingUtilities.Udp.Unicast
+{
+	public class UnicastServer : AbstractServer
+	{
+		public UnicastServer(string ip, int port, string interfaceName) : base(ip, port, interfaceName)
+		{
+		}
+
+		public override void Send(string message, string to = "")
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void StopService()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void StartService()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
