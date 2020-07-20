db10e258c7463b23238c75982f84b27d24da2a5d
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-07-21 01:17:09 +0200

    Updated Client module for lab6

--- samples/Lab6/UdpClient/ViewModels/ViewModelBase.cs
+++ samples/Lab6/UdpClient/ViewModels/ViewModelBase.cs
@@ -5,19 +5,19 @@ using ReactiveUI;
 
 namespace UdpClient.ViewModels
 {
-   public class ViewModelBase : ReactiveObject
-   {
-      public ReactiveCommand<CancelEventArgs, Unit> ClosingCommand { get; }
-      
-      public ViewModelBase()
-      {
-         ClosingCommand = ReactiveCommand.Create<CancelEventArgs>(ExecuteClosing);
-      }
+	public class ViewModelBase : ReactiveObject
+	{
+		public ReactiveCommand<CancelEventArgs, Unit> ClosingCommand { get; }
 
-      protected virtual void ExecuteClosing(CancelEventArgs args)
-      {
-         args.Cancel = false;
-         ((ClassicDesktopStyleApplicationLifetime)Avalonia.Application.Current.ApplicationLifetime).Shutdown(0);
-      }
-   }
+		public ViewModelBase()
+		{
+			ClosingCommand = ReactiveCommand.Create<CancelEventArgs>(ExecuteClosing);
+		}
+
+		protected virtual void ExecuteClosing(CancelEventArgs args)
+		{
+			args.Cancel = false;
+			((ClassicDesktopStyleApplicationLifetime)Avalonia.Application.Current.ApplicationLifetime).Shutdown(0);
+		}
+	}
 }
