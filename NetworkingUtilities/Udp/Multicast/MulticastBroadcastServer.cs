cc978f07528ac1e0cf104cd039ce56ff017db441
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-07-21 22:12:17 +0200

    Added base of MulticastBroadcastServer

---
+++ NetworkingUtilities/Udp/Multicast/MulticastBroadcastServer.cs
@@ -0,0 +1,27 @@
+﻿using NetworkingUtilities.Abstracts;
+
+namespace NetworkingUtilities.Udp.Multicast
+{
+	class MulticastBroadcastServer : AbstractServer, IReceiver
+	{
+		public MulticastBroadcastServer(string ip, int port, string interfaceName) : base(ip, port, interfaceName)
+		{
+		}
+
+		public override void Send(string message, string to = "")
+		{
+		}
+
+		public override void StopService()
+		{
+		}
+
+		public override void StartService()
+		{
+		}
+
+		public void Receive()
+		{
+		}
+	}
+}
