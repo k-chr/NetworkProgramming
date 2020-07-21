e433b5478589b36727981a398c86406bc3c58ccd
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-07-22 01:59:00 +0200

    Implemented Send and Receive for MulticastBroadcastServer

--- NetworkingUtilities/Udp/Multicast/MulticastBroadcastServer.cs
+++ NetworkingUtilities/Udp/Multicast/MulticastBroadcastServer.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using NetworkingUtilities.Abstracts;
 using NetworkingUtilities.Extensions;
 using NetworkingUtilities.Utilities.Events;
@@ -20,10 +23,6 @@ namespace NetworkingUtilities.Udp.Multicast
 			_acceptBroadcast = acceptBroadcast;
 			ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 			_clientsBuffers = new Dictionary<EndPoint, ControlState>();
-		}
-
-		public override void Send(string message, string to = "")
-		{
 		}
 
 		public override void StopService() =>
@@ -82,6 +81,136 @@ namespace NetworkingUtilities.Udp.Multicast
 
 		public void Receive()
 		{
+			try
+			{
+				var state = new ControlState
+				{
+					CurrentSocket = ServerSocket,
+					BufferSize = MaxBufferSize,
+					Buffer = new byte[MaxBufferSize],
+					StreamBuffer = new MemoryStream()
+				};
+				var endPoint = new IPEndPoint(IPAddress.Any, 0) as EndPoint;
+				ServerSocket.BeginReceiveFrom(state.Buffer, 0, state.Buffer.Length, 0, ref endPoint,
+					OnReceiveFromCallback, state);
+			}
+			catch (ObjectDisposedException)
+			{
+			}
+			catch (SocketException socketException)
+			{
+				OnCaughtException(socketException, EventCode.Receive);
+			}
+			catch (Exception exception)
+			{
+				OnCaughtException(exception, EventCode.Other);
+			}
+		}
+
+		private void OnReceiveFromCallback(IAsyncResult ar)
+		{
+			try
+			{
+				var end = new IPEndPoint(IPAddress.Any, 0) as EndPoint;
+				if (!(ar.AsyncState is ControlState state)) return;
+				var bytesRead = state.CurrentSocket.EndReceiveFrom(ar, ref end);
+
+				if (!_clientsBuffers.ContainsKey(end))
+				{
+					var s = new ControlState
+					{
+						Buffer = new byte[MaxBufferSize],
+						BufferSize = MaxBufferSize,
+						StreamBuffer = new MemoryStream(),
+					};
+					_clientsBuffers.Add(end, s);
+				}
+
+				if (bytesRead > 0)
+				{
+					_clientsBuffers[end].StreamBuffer.Write(state.Buffer, 0, bytesRead);
+					if (state.Buffer.Any(@byte => @byte == '\0'))
+					{
+						ProcessMessage(end);
+						_clientsBuffers[end].StreamBuffer = new MemoryStream();
+					}
+				}
+				else if (_clientsBuffers[end].StreamBuffer.CanWrite && _clientsBuffers[end].StreamBuffer.Length > 0)
+				{
+					ProcessMessage(end);
+					_clientsBuffers[end].StreamBuffer = new MemoryStream();
+				}
+			}
+			catch (ObjectDisposedException)
+			{
+			}
+			catch (SocketException socketException)
+			{
+				OnCaughtException(socketException, EventCode.Receive);
+			}
+			catch (Exception exception)
+			{
+				OnCaughtException(exception, EventCode.Other);
+			}
+		}
+
+		private void ProcessMessage(EndPoint end)
+		{
+			if (!_clientsBuffers.ContainsKey(end)) return;
+			var state = _clientsBuffers[end];
+			using var stream = state.StreamBuffer;
+			stream.Seek(0, SeekOrigin.Begin);
+			var message = Encoding.UTF8.GetString(stream.ToArray());
+			OnNewMessage(message, ((IPEndPoint) end).ToString(), "server");
+		}
+
+		public override void Send(string message, string to = "")
+		{
+			try
+			{
+				var data = Encoding.ASCII.GetBytes(message);
+				var endpoint = IPEndPoint.Parse(to);
+				var state = new ReceiverState
+				{
+					Port = endpoint.Port,
+					Socket = ServerSocket,
+					Ip = endpoint.Address.ToString()
+				};
+
+				ServerSocket.BeginSendTo(data, 0, data.Length, SocketFlags.None, endpoint, OnSendToCallback, state);
+			}
+			catch (ObjectDisposedException)
+			{
+			}
+			catch (SocketException socketException)
+			{
+				OnCaughtException(socketException, EventCode.Send);
+			}
+			catch (Exception e)
+			{
+				OnCaughtException(e, EventCode.Other);
+			}
+		}
+
+		private void OnSendToCallback(IAsyncResult ar)
+		{
+			try
+			{
+				if (!(ar.AsyncState is ReceiverState state)) return;
+				var _ = state.Socket.EndSendTo(ar);
+				OnNewMessage($"Data were successfully sent to {state.Ip}:{state.Port}", "server", "server");
+			}
+			catch (ObjectDisposedException)
+			{
+			}
+			catch (SocketException socketException)
+			{
+				OnCaughtException(socketException, EventCode.Send);
+			}
+			catch (Exception e)
+			{
+				OnCaughtException(e, EventCode.Other);
+			}
 		}
 	}
 }
