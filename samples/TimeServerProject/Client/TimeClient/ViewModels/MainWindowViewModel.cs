fc31610edca63872fa24e50b4de5c202d0d008e0
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-07-22 03:03:34 +0200

    Added basis of Client and Server projects for TimeServerProject

---
+++ samples/TimeServerProject/Client/TimeClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeClient.ViewModels
+{
+	public class MainWindowViewModel : ViewModelBase
+	{
+		public string Greeting => "Welcome to Avalonia!";
+	}
+}
---
+++ samples/TimeServerProject/Client/TimeClient/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace TimeClient.ViewModels
+{
+	public class ViewModelBase : ReactiveObject
+	{
+	}
+}
