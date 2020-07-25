e5765003f71741a849e50a3292dce944937a8487
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-07-25 02:40:20 +0200

    Added basic TimeClient and TimeServer classes

---
+++ samples/TimeServerProject/Client/TimeClient/Services/TimeClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using NetworkingUtilities.Abstracts;
+
+namespace TimeClient.Services
+{
+	public class TimeClient : AbstractClient
+	{
+		public TimeClient(Socket clientSocket, bool serverHandler = false) : base(clientSocket, serverHandler)
+		{
+		}
+
+		public override void Send(string message, string to = "")
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Receive()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void StopService()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void StartService()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
