e366504659178b7322ad2892ae7a65a88c90b91f
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-07-27 03:50:14 +0200

    Added basis inner services of TimeClient and TimeServer, added initial methods' implementations in TimeServer

--- samples/TimeServerProject/Client/TimeClient/Services/TimeClient.cs
+++ samples/TimeServerProject/Client/TimeClient/Services/TimeClient.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
 using NetworkingUtilities.Abstracts;
+using NetworkingUtilities.Tcp;
+using NetworkingUtilities.Udp.Multicast;
 
 namespace TimeClient.Services
 {
 	public class TimeClient : AbstractClient
 	{
+		private MulticastClient _discoveryClient;
+		private Client _tcpClient;
+		
 		public TimeClient(Socket clientSocket, bool serverHandler = false) : base(clientSocket, serverHandler)
 		{
 		}
--- samples/TimeServerProject/Server/TimeServer/Services/TimeServer.cs
+++ samples/TimeServerProject/Server/TimeServer/Services/TimeServer.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NetworkingUtilities.Abstracts;
+using NetworkingUtilities.Tcp;
+using NetworkingUtilities.Udp.Multicast;
 
 namespace TimeServer.Services
 {
-	public class TimeServer
+	public class TimeServer : AbstractServer
 	{
+		private MulticastBroadcastServer _multicastBroadcastServer;
+		private MultithreadingServer _tcpServer;
+
+		public TimeServer(string ip, int port, string interfaceName) : base(ip, port, interfaceName)
+		{
+		}
+
+		public override void Send(string message, string to = "")
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void StopService()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void StartService()
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
