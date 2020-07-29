9e432178b1fafcf971e92b7396d57b45f12be049
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-07-29 18:22:36 +0200

    Added ConfigView and ServerSelectionView for Client

---
+++ samples/TimeServerProject/Client/TimeClient/Views/ServerSelectionView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TimeClient.Views
+{
+	public class ServerSelectionView : UserControl
+	{
+		public ServerSelectionView()
+		{
+			this.InitializeComponent();
+		}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+	}
+}
