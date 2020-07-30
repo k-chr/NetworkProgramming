631ff755279093dae2381239a5e0c58ec71a8e97
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-07-30 22:55:50 +0200

    Prepared Fluent-style window

--- samples/TimeServerProject/Client/TimeClient/Views/MainWindow.xaml.cs
+++ samples/TimeServerProject/Client/TimeClient/Views/MainWindow.xaml.cs
@@ -1,17 +1,54 @@
-﻿using Avalonia;
+﻿using System;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Markup.Xaml.Styling;
 
 namespace TimeClient.Views
 {
 	public class MainWindow : Window
 	{
+		private readonly StyleInclude _dark;
+		private readonly StyleInclude _light;
+
 		public MainWindow()
 		{
 			InitializeComponent();
 #if DEBUG
-            this.AttachDevTools();
+			this.AttachDevTools();
 #endif
+			var light = new StyleInclude(new Uri("resm:Styles?assembly=TimeClient"))
+			{
+				Source = new Uri("avares://Avalonia.Themes.Fluent/Accents/FluentLight.xaml")
+			};
+			var dark = new StyleInclude(new Uri("resm:Styles?assembly=TimeClient"))
+			{
+				Source = new Uri("avares://Avalonia.Themes.Fluent/Accents/FluentDark.xaml")
+			};
+			
+			_light = light;
+			_dark = dark;
+			Styles.Add(dark);
+		}
+
+		protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+		{
+			base.OnApplyTemplate(e);
+			ExtendClientAreaChromeHints =
+				Avalonia.Platform.ExtendClientAreaChromeHints.PreferSystemChrome |
+				Avalonia.Platform.ExtendClientAreaChromeHints.OSXThickTitleBar;
+		}
+		public void OnStyleChange(object sender, RoutedEventArgs args)
+		{
+			var button = (ToggleSwitch) sender;
+			Styles[0] = button.IsChecked switch
+						{
+							true => _light,
+							false => _dark,
+							_ => _light
+						};
 		}
 
 		private void InitializeComponent()
