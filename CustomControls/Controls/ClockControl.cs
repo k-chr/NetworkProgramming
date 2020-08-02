65aee1ed9fcadf5b013f21960885a94986f38127
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-08-02 23:08:57 +0200

    Added basis of ClockControl

---
+++ CustomControls/Controls/ClockControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Timers;
+using Avalonia;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+using Avalonia.Threading;
+
+namespace CustomControls.Controls
+{
+	public class ClockControl : TemplatedControl
+	{
+		private const int Interval = 1000;
+
+		private double HourAngle
+		{
+			set => SetValue(HoursAngleProperty, value);
+		}
+
+		private double MinutesAngle
+		{
+			set => SetValue(HoursAngleProperty, value);
+		}
+
+		private double SecondsAngle
+		{
+			set => SetValue(HoursAngleProperty, value);
+		}
+
+		public StyledProperty<double> HoursAngleProperty;
+		public StyledProperty<double> SecondsAngleProperty;
+		public StyledProperty<double> MinutesAngleProperty;
+
+		public ClockControl()
+		{
+			var clockTimer = new Timer(Interval);
+			clockTimer.Elapsed += (sender, args) => { Dispatcher.UIThread.InvokeAsync(() => { }); };
+
+			HoursAngleProperty =
+				AvaloniaProperty.Register<ClockControl, double>(nameof(HoursAngleProperty), 0, false,
+					BindingMode.TwoWay);
+			SecondsAngleProperty =
+				AvaloniaProperty.Register<ClockControl, double>(nameof(SecondsAngleProperty), 0, false,
+					BindingMode.TwoWay);
+			MinutesAngleProperty =
+				AvaloniaProperty.Register<ClockControl, double>(nameof(MinutesAngleProperty), 0, false,
+					BindingMode.TwoWay);
+		}
+	}
+}
