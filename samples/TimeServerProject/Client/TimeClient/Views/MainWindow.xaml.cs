c6515ffa65967fb1cfeaa9d84f89685d0ede5d06
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-08-04 01:49:22 +0200

    Repaired bug with switching tabs of MainWindow

--- samples/TimeServerProject/Client/TimeClient/Views/MainWindow.xaml.cs
+++ samples/TimeServerProject/Client/TimeClient/Views/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
 
 namespace TimeClient.Views
 {
@@ -13,6 +14,41 @@ namespace TimeClient.Views
 			var themes = this.Find<ToggleSwitch>("Themes");
 			themes.Checked += (sender, args) => Application.Current.Styles[0] = App.FluentLight;
 			themes.Unchecked += (sender, args) => Application.Current.Styles[0] = App.FluentDark;
+			var tabControl = this.Find<TabControl>("TabControl");
+			tabControl.SelectionChanged += (sender, args) =>
+			{
+				var key = "PART_ItemsContentPresenter";
+				var children = tabControl.GetVisualChildren();
+				foreach (var child in children)
+				{
+					if (child is ScrollViewer viewer && (viewer.Name?.Equals(key) ?? false))
+					{
+						viewer.ScrollToHome();
+						break;
+					}
+
+					if (!(Traverse(child, key) is ScrollViewer scrollViewer)) continue;
+					scrollViewer.ScrollToHome();
+					break;
+				}
+			};
+		}
+
+		private static IVisual Traverse(IVisual child, string key)
+		{
+			if (child is ScrollViewer viewer && (viewer.Name?.Equals(key) ?? false))
+			{
+				return child;
+			}
+
+			IVisual result = null;
+
+			foreach (var visualChild in child.GetVisualChildren())
+			{
+				result = Traverse(visualChild, key);
+			}
+
+			return result;
 		}
 
 		private void InitializeComponent()
