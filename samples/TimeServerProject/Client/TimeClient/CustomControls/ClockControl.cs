e5293a89c9c3c683e0fc11855b98a50033be1a09
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-08-03 18:55:20 +0200

    Modified DirectProperties of ClockControl. Fixed bugs with ticks' placement

--- samples/TimeServerProject/Client/TimeClient/CustomControls/ClockControl.cs
+++ samples/TimeServerProject/Client/TimeClient/CustomControls/ClockControl.cs
@@ -50,10 +50,26 @@ namespace TimeClient.CustomControls
 		public double Radius
 		{
 			get => GetValue(RadiusProperty);
-			set => SetValue(RadiusProperty, value);
+			set
+			{
+				SetValue(RadiusProperty, value);
+				HoursLength = Radius * 0.5;
+				MinutesLength = Radius * 0.8;
+				SecondsLength = Radius * 0.9;
+				TickWidth = Radius / 40;
+				SecondsTickWidth = Radius / 60;
+				HoursTransformOrigin = new RelativePoint(TickWidth / 2, HoursLength, RelativeUnit.Absolute);
+				MinutesTransformOrigin = new RelativePoint(TickWidth / 2, MinutesLength, RelativeUnit.Absolute);
+				SecondsTransformOrigin = new RelativePoint(SecondsTickWidth / 2, SecondsLength, RelativeUnit.Absolute);
+				HoursTickOffset = Thickness.Parse($"0 0 0 {HoursLength}");
+				MinutesTickOffset = Thickness.Parse($"0 0 0 {MinutesLength}");
+				SecondsTickOffset = Thickness.Parse($"0 0 0 {SecondsLength}");
+			}
 		}
 
-		public static readonly DirectProperty<ClockControl, Thickness> TickOffsetProperty;
+		public static readonly DirectProperty<ClockControl, Thickness> HoursTickOffsetProperty;
+		public static readonly DirectProperty<ClockControl, Thickness> MinutesTickOffsetProperty;
+		public static readonly DirectProperty<ClockControl, Thickness> SecondsTickOffsetProperty;
 
 		public static readonly StyledProperty<double> RadiusProperty;
 		public static readonly DirectProperty<ClockControl, double> DiameterProperty;
@@ -67,11 +83,93 @@ namespace TimeClient.CustomControls
 		public static readonly DirectProperty<ClockControl, double> SecondsLengthProperty;
 
 		public static readonly DirectProperty<ClockControl, double> TickWidthProperty;
-		public static readonly DirectProperty<ClockControl, double> SecondTickWidthProperty;
+		public static readonly DirectProperty<ClockControl, double> SecondsTickWidthProperty;
+
+		public static readonly DirectProperty<ClockControl, RelativePoint> HoursTransformOriginProperty;
+		public static readonly DirectProperty<ClockControl, RelativePoint> MinutesTransformOriginProperty;
+		public static readonly DirectProperty<ClockControl, RelativePoint> SecondsTransformOriginProperty;
 
 		public static readonly StyledProperty<double> HoursAngleProperty;
 		public static readonly StyledProperty<double> SecondsAngleProperty;
 		public static readonly StyledProperty<double> MinutesAngleProperty;
+
+		private double _hoursLength;
+		private double _minutesLength;
+		private double _secondsLength;
+		private RelativePoint _hoursTransformOrigin;
+		private RelativePoint _minutesTransformOrigin;
+		private RelativePoint _secondsTransformOrigin;
+		private double _tickWidth;
+		private double _secondsTickWidth;
+		private Thickness _hoursTickOffset;
+		private Thickness _minutesTickOffset;
+		private Thickness _secondsTickOffset;
+
+		public double SecondsLength
+		{
+			get => _secondsLength;
+			set => SetAndRaise(SecondsLengthProperty, ref _secondsLength, value);
+		}
+
+		public double MinutesLength
+		{
+			get => _minutesLength;
+			set => SetAndRaise(MinutesLengthProperty, ref _minutesLength, value);
+		}
+
+		public double HoursLength
+		{
+			get => _hoursLength;
+			set => SetAndRaise(HoursLengthProperty, ref _hoursLength, value);
+		}
+
+		public RelativePoint SecondsTransformOrigin
+		{
+			get => _secondsTransformOrigin;
+			set => SetAndRaise(SecondsTransformOriginProperty, ref _secondsTransformOrigin, value);
+		}
+
+		public RelativePoint MinutesTransformOrigin
+		{
+			get => _minutesTransformOrigin;
+			set => SetAndRaise(MinutesTransformOriginProperty, ref _minutesTransformOrigin, value);
+		}
+
+		public RelativePoint HoursTransformOrigin
+		{
+			get => _hoursTransformOrigin;
+			set => SetAndRaise(HoursTransformOriginProperty, ref _hoursTransformOrigin, value);
+		}
+
+		public Thickness SecondsTickOffset
+		{
+			get => _secondsTickOffset;
+			set => SetAndRaise(SecondsTickOffsetProperty, ref _secondsTickOffset, value);
+		}
+
+		public Thickness MinutesTickOffset
+		{
+			get => _minutesTickOffset;
+			set => SetAndRaise(MinutesTickOffsetProperty, ref _minutesTickOffset, value);
+		}
+
+		public Thickness HoursTickOffset
+		{
+			get => _hoursTickOffset;
+			set => SetAndRaise(HoursTickOffsetProperty, ref _hoursTickOffset, value);
+		}
+
+		public double SecondsTickWidth
+		{
+			get => _secondsTickWidth;
+			set => SetAndRaise(SecondsTickWidthProperty, ref _secondsTickWidth, value);
+		}
+
+		public double TickWidth
+		{
+			get => _tickWidth;
+			set => SetAndRaise(TickWidthProperty, ref _tickWidth, value);
+		}
 
 		static ClockControl()
 		{
@@ -100,21 +198,41 @@ namespace TimeClient.CustomControls
 			DiameterProperty =
 				AvaloniaProperty.RegisterDirect<ClockControl, double>("Diameter", control => control.Radius * 2);
 
-			TickOffsetProperty = AvaloniaProperty.RegisterDirect<ClockControl, Thickness>("TickOffset",
-				control => new Thickness(0, 0, 0, control.Radius / 2));
-
 			TickWidthProperty =
-				AvaloniaProperty.RegisterDirect<ClockControl, double>("TickWidth", control => control.Radius / 40);
-			SecondTickWidthProperty =
-				AvaloniaProperty.RegisterDirect<ClockControl, double>("SecondTickWidth",
-					control => control.Radius / 60);
+				AvaloniaProperty.RegisterDirect<ClockControl, double>(nameof(TickWidth), control => control.TickWidth);
+			SecondsTickWidthProperty =
+				AvaloniaProperty.RegisterDirect<ClockControl, double>(nameof(SecondsTickWidth),
+					control => control.SecondsTickWidth);
 
 			HoursLengthProperty =
-				AvaloniaProperty.RegisterDirect<ClockControl, double>("HoursLength", control => control.Radius * 0.5);
+				AvaloniaProperty.RegisterDirect<ClockControl, double>(nameof(HoursLength),
+					control => control.HoursLength);
 			MinutesLengthProperty =
-				AvaloniaProperty.RegisterDirect<ClockControl, double>("MinutesLength", control => control.Radius * 0.8);
+				AvaloniaProperty.RegisterDirect<ClockControl, double>(nameof(MinutesLength),
+					control => control.MinutesLength);
 			SecondsLengthProperty =
-				AvaloniaProperty.RegisterDirect<ClockControl, double>("SecondsLength", control => control.Radius * 0.9);
+				AvaloniaProperty.RegisterDirect<ClockControl, double>(nameof(SecondsLength),
+					control => control.SecondsLength);
+
+			HoursTransformOriginProperty = AvaloniaProperty.RegisterDirect<ClockControl, RelativePoint>(
+				nameof(HoursTransformOrigin),
+				control => control.HoursTransformOrigin);
+			MinutesTransformOriginProperty = AvaloniaProperty.RegisterDirect<ClockControl, RelativePoint>(
+				nameof(MinutesTransformOrigin),
+				control => control.MinutesTransformOrigin);
+			SecondsTransformOriginProperty = AvaloniaProperty.RegisterDirect<ClockControl, RelativePoint>(
+				nameof(SecondsTransformOrigin),
+				control => control.SecondsTransformOrigin);
+
+			HoursTickOffsetProperty =
+				AvaloniaProperty.RegisterDirect<ClockControl, Thickness>(nameof(HoursTickOffset),
+					control => control.HoursTickOffset);
+			MinutesTickOffsetProperty =
+				AvaloniaProperty.RegisterDirect<ClockControl, Thickness>(nameof(MinutesTickOffset),
+					control => control.MinutesTickOffset);
+			SecondsTickOffsetProperty =
+				AvaloniaProperty.RegisterDirect<ClockControl, Thickness>(nameof(SecondsTickOffset),
+					control => control.SecondsTickOffset);
 		}
 
 		public ClockControl()
