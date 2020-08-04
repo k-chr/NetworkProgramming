7c89c7a0c8411a65a52097d791f68cf4d3ee1da1
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-08-04 14:19:42 +0200

    Implemented basis of ServerSelectionView. Added ServerModel

---
+++ samples/TimeServerProject/Client/TimeClient/Models/ServerModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+namespace TimeClient.Models
+{
+	public class ServerModel
+	{
+		public static implicit operator ServerModel(ValueTuple<IPEndPoint, string> pair) =>
+			Create(pair.Item1, pair.Item2);
+
+		public static ServerModel Create(IPEndPoint pairItem1, string pairItem2) =>
+			new ServerModel
+			{
+				Ip = pairItem1,
+				Name = pairItem2
+			};
+
+		public IPEndPoint Ip { get; private set; }
+
+		public string Name { get; private set; }
+
+		public override string ToString() => $"{Name}|{Ip}";
+	}
+}
