ccbc99742cc1cc2493e593521a7b997917a41639
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-08-08 21:00:17 +0200

    Added StatusEvent, StatusReporter and StatusCode for handling statuses, miscellaneous messages, basic logging etc, to add ability to get notification about general event before ExceptionReporter, MessageReporter or ClientReporter notified any Subscriber about details

---
+++ NetworkingUtilities/Publishers/StatusReporter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NetworkingUtilities.Abstracts;
+using NetworkingUtilities.Utilities.Events;
+
+namespace NetworkingUtilities.Publishers
+{
+	public class StatusReporter : IReporter, IReporter<StatusEvent>
+	{
+		public event EventHandler<StatusEvent> Report;
+
+		public void Notify(object obj)
+		{
+			if (obj is ValueTuple<StatusCode, string> data)
+				Report?.Invoke(this, new StatusEvent(data.Item1, data.Item2));
+		}
+
+		public void AddSubscriber(Action<object, object> procedure) => Report += (s, e) => procedure?.Invoke(s, e);
+	}
+}
---
+++ NetworkingUtilities/Utilities/Events/StatusEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace NetworkingUtilities.Utilities.Events
+{
+	public class StatusEvent
+	{
+		public StatusCode StatusCode { get; }
+		public string StatusMessage { get; }
+
+		public StatusEvent(StatusCode code, string message)
+		{
+			StatusCode = code;
+			StatusMessage = message;
+		}
+	}
+}
