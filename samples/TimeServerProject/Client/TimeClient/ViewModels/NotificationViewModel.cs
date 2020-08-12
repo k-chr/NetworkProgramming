fcd57cfcef25646cfe6849f139d3535cd889983f
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-08-12 22:19:12 +0200

    Added basis of notifications system and views.

---
+++ samples/TimeServerProject/Client/TimeClient/ViewModels/NotificationViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Avalonia.Controls.Shapes;
+
+namespace TimeClient.ViewModels
+{
+	public abstract class NotificationViewModel : ViewModelBase
+	{
+		public abstract Path Path { get; set; }
+	}
+
+	class InfoNotificationViewModel : NotificationViewModel
+	{
+		public override Path Path { get; set; }
+	}
+
+	class ErrorNotificationViewModel : NotificationViewModel
+	{
+		public override Path Path { get; set; }
+	}
+
+	class SuccessNotificationViewModel : NotificationViewModel
+	{
+		public override Path Path { get; set; }
+	}
+
+	public static class NotificationViewModelFactory
+	{
+
+	}
+}
---
+++ samples/TimeServerProject/Client/TimeClient/Views/NotificationView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TimeClient.Views
+{
+	public class NotificationView : UserControl
+	{
+		public NotificationView()
+		{
+			this.InitializeComponent();
+		}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+	}
+}
