69f9d80ca1dc5f9eba6f335d33081cc14895cbe7
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-08-15 17:36:57 +0200

    Added animations for SideBar, struggling with FocusAdorner bug

--- samples/TimeServerProject/Client/TimeClient/Views/MainWindow.xaml.cs
+++ samples/TimeServerProject/Client/TimeClient/Views/MainWindow.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Notifications;
+using Avalonia.Data;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using Avalonia.VisualTree;
+using DynamicData;
+using JetBrains.Annotations;
 
 namespace TimeClient.Views
 {
@@ -10,39 +14,79 @@ namespace TimeClient.Views
 	{
 		public WindowNotificationManager NotificationArea { get; }
 
+		public static readonly StyledProperty<bool> SideBarCollapsedProperty;
+		private bool _sideBarCollapsed;
+
+		public bool SideBarCollapsed
+		{
+			get => _sideBarCollapsed;
+			set => SetAndRaise(SideBarCollapsedProperty, ref _sideBarCollapsed, value);
+		}
+
 		public MainWindow()
 		{
 			InitializeComponent();
 			this.AttachDevTools();
 			var themes = this.Find<ToggleSwitch>("Themes");
-			themes.Checked += (sender, args) => Application.Current.Styles[0] = App.FluentLight;
-			themes.Unchecked += (sender, args) => Application.Current.Styles[0] = App.FluentDark;
+
 			var tabControl = this.Find<TabControl>("TabControl");
+
+			themes.Checked += (sender, args) =>
+			{
+				Application.Current.Styles[0] = App.FluentLight;
+				var key2 = "PART_ItemsHeadersPresenter";
+				var scrollViewer2 = Traverse<ScrollViewer>(tabControl, key2) as ScrollViewer;
+				scrollViewer2?.ScrollToHome();
+			};
+
+			themes.Unchecked += (sender, args) =>
+			{
+				Application.Current.Styles[0] = App.FluentDark;
+				var key2 = "PART_ItemsHeadersPresenter";
+				var scrollViewer2 = Traverse<ScrollViewer>(tabControl, key2) as ScrollViewer;
+				scrollViewer2?.ScrollToHome();
+			};
+
+			Classes.Add("sideBarCollapsed");
 
 			tabControl.SelectionChanged += (sender, args) =>
 			{
 				var key = "PART_ItemsContentPresenter";
-				var children = tabControl.GetVisualChildren();
-				foreach (var child in children)
-				{
-					if (child is ScrollViewer viewer && (viewer.Name?.Equals(key) ?? false))
-					{
-						viewer.ScrollToHome();
-						break;
-					}
-
-					if (!(Traverse<ScrollViewer>(child, key) is ScrollViewer scrollViewer)) continue;
-					scrollViewer.ScrollToHome();
-					break;
-				}
+				var scrollViewer = Traverse<ScrollViewer>(tabControl, key) as ScrollViewer;
+				scrollViewer?.ScrollToHome();
 			};
 
+			var sideBar = this.Find<ExperimentalAcrylicBorder>("SideBar");
+			sideBar.PointerEnter += OnPointerEnter;
+			PointerLeave += OnPointerLeave;
+
+			var content = this.Find<ExperimentalAcrylicBorder>("Content");
+
+			content.PointerEnter += OnPointerLeave;
 			NotificationArea = new WindowNotificationManager(this)
 			{
 				Position = NotificationPosition.TopRight,
 				MaxItems = 3
 			};
 		}
+
+		private void OnPointerEnter([CanBeNull] object sender, PointerEventArgs args)
+		{
+			if (!(args.Source is ExperimentalAcrylicBorder)) return;
+			Classes.ReplaceOrAdd("sideBarCollapsed", "sideBarShown");
+			SideBarCollapsed = false;
+		}
+
+		private void OnPointerLeave([CanBeNull] object sender, PointerEventArgs args)
+		{
+			if (!(args.Source is ExperimentalAcrylicBorder || args.Source is MainWindow)) return;
+			Classes.ReplaceOrAdd("sideBarShown", "sideBarCollapsed");
+			SideBarCollapsed = true;
+		}
+
+		static MainWindow() =>
+			SideBarCollapsedProperty = AvaloniaProperty.Register<MainWindow, bool>("SideBarCollapsed",
+				false, false, BindingMode.TwoWay);
 
 		private static IVisual Traverse<T>(IVisual child, string key) where T : Control
 		{
@@ -56,6 +100,7 @@ namespace TimeClient.Views
 			foreach (var visualChild in child.GetVisualChildren())
 			{
 				result = Traverse<T>(visualChild, key);
+				if (result != null) break;
 			}
 
 			return result;
