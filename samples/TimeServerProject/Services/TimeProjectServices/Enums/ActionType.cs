beb2fc20b307d28d871080a49d3d4b1ec6047350
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-08-17 14:31:03 +0200

    Added TimeProjectServices project to extract shared abstraction between Server and Client.

---
+++ samples/TimeServerProject/Services/TimeProjectServices/Enums/ActionType.cs
@@ -0,0 +1,8 @@
+﻿namespace TimeProjectServices.Enums
+{
+	public enum ActionType
+	{
+		Query = 0xDEAD,
+		Response = 0xBEEF
+	}
+}
---
+++ samples/TimeServerProject/Services/TimeProjectServices/Enums/HeaderType.cs
@@ -0,0 +1,8 @@
+﻿namespace TimeProjectServices.Enums
+{
+	public enum HeaderType
+	{
+		Discover = 0xDEAD,
+		Time = 0xBEEF
+	}
+}
