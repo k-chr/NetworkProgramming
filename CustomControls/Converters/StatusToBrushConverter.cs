10849fe67cebded8b79319efbc301aa2d3fdebc8
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-08-18 15:06:27 +0200

    Implemented StatusToBrushConverter for notifications

---
+++ CustomControls/Converters/StatusToBrushConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using NetworkingUtilities.Utilities.Events;
+
+namespace CustomControls.Converters
+{
+	public class StatusToBrushConverter : IValueConverter
+	{
+		
+		private static readonly Color DarkRed = Colors.DarkRed;
+		private static readonly Color DarkGreen = Colors.DarkGreen;
+		private static readonly Color DarkBlue = Colors.DarkBlue;
+
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is StatusCode code)
+			{
+				return code switch
+					   {
+						   StatusCode.Error => DarkRed,
+						   StatusCode.Success => DarkGreen,
+						   StatusCode.Info => DarkBlue,
+						   _ => throw new ArgumentOutOfRangeException()
+					   };
+			}
+
+			return value;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is Color color)
+			{
+				return color switch
+					   {
+						   { } when color.Equals(DarkRed) => StatusCode.Error,
+						   { } when color.Equals(DarkBlue) => StatusCode.Info,
+						   { } when color.Equals(DarkGreen) => StatusCode.Success,
+						   _ => throw new ArgumentOutOfRangeException()
+					   };
+			}
+
+			return value;
+		}
+	}
+}
