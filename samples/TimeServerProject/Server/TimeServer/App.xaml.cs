396f5a0bed4ecb933550b98c67ac7ea44f99ce9b
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-08-24 18:33:14 +0200

    Implemented App of TimeServer based on class contained in TimeClient project

--- samples/TimeServerProject/Server/TimeServer/App.xaml.cs
+++ samples/TimeServerProject/Server/TimeServer/App.xaml.cs
@@ -1,6 +1,13 @@
-﻿using Avalonia;
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Markup.Xaml.Styling;
+using Avalonia.ReactiveUI;
+using Avalonia.Styling;
+using ReactiveUI;
+using TimeServer.Services;
 using TimeServer.ViewModels;
 using TimeServer.Views;
 
@@ -8,19 +15,54 @@ namespace TimeServer
 {
 	public class App : Application
 	{
+		private const string ConfigFileName = "Config.bin";
+		private AutoSuspendHelper _suspendHelper;
+
+		public static readonly Styles FluentDark = new Styles
+		{
+			new StyleInclude(new Uri("resm:Styles?assembly=TimeProjectServices"))
+			{
+				Source = new Uri("avares://TimeProjectServices/Styles/FluentDark.xaml")
+			},
+		};
+
+		public static readonly Styles FluentLight = new Styles
+		{
+			new StyleInclude(new Uri("resm:Styles?assembly=TimeProjectServices"))
+			{
+				Source = new Uri("avares://TimeProjectServices/Styles/FluentLight.xaml")
+			},
+		};
+
 		public override void Initialize()
 		{
 			AvaloniaXamlLoader.Load(this);
+			_suspendHelper = new AutoSuspendHelper(ApplicationLifetime);
+			RxApp.SuspensionHost.CreateNewAppState = () => new ConfigViewModel();
+			RxApp.SuspensionHost.SetupDefaultSuspendResume(new BinaryConfigurationSuspensionDriver(ConfigFileName));
 		}
 
 		public override void OnFrameworkInitializationCompleted()
 		{
 			if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
 			{
-				desktop.MainWindow = new MainWindow
+				_suspendHelper.OnFrameworkInitializationCompleted();
+				var mainWindow = new MainWindow();
+
+				var viewModel = new MainWindowViewModel(mainWindow.NotificationArea,
+					RxApp.SuspensionHost.GetAppState<ConfigViewModel>());
+
+				mainWindow.DataContext = viewModel;
+				desktop.MainWindow = mainWindow;
+
+				Window.WindowClosedEvent.AddClassHandler(typeof(MainWindow), (sender, args) =>
 				{
-					DataContext = new MainWindowViewModel(),
-				};
+					if (sender is MainWindow window)
+					{
+						var mainWindowViewModel = window.DataContext as MainWindowViewModel;
+						mainWindowViewModel?.OnClosing();
+					}
+				});
 			}
 
 			base.OnFrameworkInitializationCompleted();
