94e2909be6a1739079b18d53ced9ef1421981bd3
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-08-28 23:43:48 +0200

    Improved ServersView, added LogDumper to write all logs into file asynchronously (pushing logs to collection caused OutOfMemoryException)

--- samples/TimeServerProject/Server/TimeServer/Views/ServersView.axaml.cs
+++ samples/TimeServerProject/Server/TimeServer/Views/ServersView.axaml.cs
@@ -1,10 +1,30 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
 using Avalonia.Markup.Xaml;
+using JetBrains.Annotations;
 
 namespace TimeServer.Views
 {
 	public class ServersView : UserControl
 	{
+		[UsedImplicitly] 
+		public static readonly StyledProperty<double> InfoWidthProperty =
+			AvaloniaProperty.Register<ServersView, double>(nameof(InfoWidth), double.NaN);
+
+		private double _infoWidth;
+
+		public double InfoWidth
+		{
+			get => _infoWidth;
+			set
+			{
+				_infoWidth = value - 70;
+				RaisePropertyChanged(InfoWidthProperty, new Optional<double>(InfoWidthProperty.CoerceValue(this, 0)),
+					new BindingValue<double>(_infoWidth));
+			}
+		}
+
 		public ServersView() => InitializeComponent();
 
 		private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
---
+++ samples/TimeServerProject/Services/TimeProjectServices/Services/LogDumper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using CustomControls.Models;
+
+namespace TimeProjectServices.Services
+{
+	public class LogDumper
+	{
+		private readonly string _path;
+		private static readonly ReaderWriterLock ReaderWriterLock = new ReaderWriterLock();
+		public LogDumper(string path) : this(ref path) => _path = path;
+
+		private LogDumper(ref string path) =>
+			File.WriteAllTextAsync(path, $"[Info] {DateTime.Now} Started logging session");
+
+		public async void DumpLog(InternalMessageModel log)
+		{
+			try
+			{
+				ReaderWriterLock.AcquireWriterLock(1000);
+				await File.AppendAllTextAsync(_path, log.ToString());
+			}
+			catch (Exception)
+			{
+				//ignored
+			}
+			finally
+			{
+				try
+				{
+					ReaderWriterLock.ReleaseWriterLock();
+				}
+				catch (Exception e)
+				{
+					//ignored
+					Console.WriteLine(e);
+				}
+			}
+		}
+
+		public async void End() =>
+			await File.AppendAllTextAsync(_path, $"[Info] {DateTime.Now} Disposed logging session");
+	}
+}
